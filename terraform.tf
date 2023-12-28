30da2189e7f264307f25074a8c47f8b960628606
Nehal Gandhi <gandhi3nehal@gmail.com>
2023-12-28 07:09:26 -0500

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = var.aws_version
+      version = "${var.aws_version}"
     }
   }
   required_version = ">= ${var.tf_version}"
